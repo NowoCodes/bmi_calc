6e894627210700ada63fa83906e34006cd0f1ec6
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-16 23:01:42 +0000

    Rename imports

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,12 +1,11 @@
-import 'package:bmi_calculator/calculator_brain.dart';
-import 'package:bmi_calculator/components/bottom_button.dart';
-import 'package:bmi_calculator/components/icon_content.dart';
-import 'package:bmi_calculator/components/reusable_card.dart';
-import 'package:bmi_calculator/components/round_icon_button.dart';
-import 'package:bmi_calculator/constants.dart';
-import 'package:bmi_calculator/screens/results_page.dart';
+import 'package:bmi_calc/calculator_brain.dart';
+import 'package:bmi_calc/components/bottom_button.dart';
+import 'package:bmi_calc/components/icon_content.dart';
+import 'package:bmi_calc/components/reusable_card.dart';
+import 'package:bmi_calc/components/round_icon_button.dart';
+import 'package:bmi_calc/constants.dart';
+import 'package:bmi_calc/screens/results_page.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 enum Gender {
   male,
